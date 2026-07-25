2f2f9f1163f6b203ab47a4549cd570aea0624a9e
Sourav-Ghosh10 <souravghoshmgu1@gmail.com>
2026-07-26 04:46:06 +0530

    26-07-2026-3

--- routes/web.php
+++ routes/web.php
@@ -264,5 +264,9 @@ Route::get('/start-websockets', function() {
     exec("nohup php $artisan websockets:serve > /dev/null 2>&1 &");
     return "WebSocket server started in the background!";
 });
+Route::get('/run-composer', function() {
+    exec("composer install 2>&1", $output);
+    return implode("<br>", $output);
+});
 
 //===================================================================================================================
